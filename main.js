32e5e41db647006198d2493eb249d48fee7b2df9
Pat Wilken <patrickwilken28@gmail.com>
2017-01-27 21:05:55 -0500

    Shapes now move (not randomly)
    - Shapes now move (and rotate! whoo!)
    - Shapes removed after 2 seconds
    - Moved all js code inside click event

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-var elems = "<div id='explosion-container'>";
-for(var i=0; i<20; i++){
-    elems += "<div class='hex'></div>"
-}
-elems += "</div>";
-
 $("html").click(function(e){
-	var id = Math.floor(Math.random()*89999+10000);
-	var x = (e.pageX - (25/2)) + 'px';
+    var id = Math.floor(Math.random()*89999+10000);
+    var x = (e.pageX - (25/2)) + 'px';
     var y = (e.pageY - (25/2)) + 'px';
     var div = $("<div id='" + id + "'>").css({
         "position": "absolute",                    
         "left": x,
         "top": y
     });
-    
+
+    var elems = "<div id='explosion-container'>";
+    for(var i=0; i<1; i++){
+        elems += "<div class='hex dir-1'></div>"
+    }
+    elems += "</div>";
+
     div.append(elems);
-    $(document.body).append(div);   
+    $(document.body).append(div);
+
+    setTimeout(function(){
+        $("#" + id).remove();
+    }, 2000);
 });
