996a13002700939df08b6fbd327477a19c7acd50
Pat Wilken <patrickwilken28@gmail.com>
2017-01-27 21:12:04 -0500

    Generates style element
    - Generates class styles & animations
    - Now using random classes!
    - Cleaned up the code a bit

--- main.js
+++ main.js
@@ -1,13 +1,52 @@
-var dirs = {classes: [], top: [], right: [], bottom: [], left: []};
+var classes = [];
 var tooMuch = false;
+
 $("document").ready(function() {
+	var dirs = {top: [], right: [], bottom: [], left: []};
 	for(var i=0; i<100; i++){
-    	dirs.classes.push(Math.floor(Math.random() * 100 + 1));
+    	classes.push(Math.floor(Math.random() * 100 + 1));
         dirs.top.push(Math.floor(Math.random() * 200 + 10));
         dirs.right.push(Math.floor(Math.random() * 200 + 10));
         dirs.bottom.push(Math.floor(Math.random() * 200 + 10));
         dirs.left.push(Math.floor(Math.random() * 200 + 10));
     }
+    
+    var styles = "<style>";
+    for(var i=0; i<100; i++){
+    	styles += ".dir-" + (i+1) + "{";
+        styles += "animation-name: dir-" + (i+1) + ";}";
+    }
+    for(var i=0; i<100; i++){
+    	var rand = {1: (Math.floor(Math.random() * 2 + 1)), 2: Math.floor(Math.random() * 100 + 1)};
+    
+    	styles += "@keyframes dir-" + (i+1) + "{";
+        for(var j=0; j<2; j++){
+        	if(j == 0){
+            	styles += "0% {transform: rotate(0deg);";
+            }
+            else{
+            	styles += "100% {transform: rotate(360deg);";
+            }
+            
+            if(rand.1 == 1){
+                styles += "top: " + dirs.top[i] + "px;";
+            }
+            else{
+				styles += "bottom: " + dirs.bottom[i] + "px;";
+            }
+            if(rand.2 == 1){
+				styles += "right: " + dirs.right[i] + "px;";
+            }
+            else{
+				styles += "left: " + dirs.left[i] + "px;";
+            }
+            styles += "}";
+        }
+        styles += "}";
+    }
+    styles += "</style>";
+    
+    $(document.head).append(styles);
 });
 
 $("html").click(function(e) {
@@ -23,7 +62,9 @@ $("html").click(function(e) {
 
         var elems = "<div id='explosion-container'>";
         for (var i = 0; i < 1; i++) {
-            elems += "<div class='hex dir-1'></div>"
+            elems += "<div class='hex dir-";
+            elems += Math.floor(Math.random() * 100 + 1);
+            elems += "'></div>";
         }
         elems += "</div>";
 
