1ab6a612545558480b0c5322c21dd4e09ca2da2d
Pat Wilken <patrickwilken28@gmail.com>
2017-01-27 21:01:28 -0500

    Displays shape on click
    - Displays a line of shapes where the user clicked

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var elems = "<div id='explosion-container'>";
-elems += "<div class='hex'></div>"
+for(var i=0; i<20; i++){
+    elems += "<div class='hex'></div>"
+}
 elems += "</div>";
 
-$("html").click(function(){
-    alert("hi");
+$("html").click(function(e){
+	var id = Math.floor(Math.random()*89999+10000);
+	var x = (e.pageX - (25/2)) + 'px';
+    var y = (e.pageY - (25/2)) + 'px';
+    var div = $("<div id='" + id + "'>").css({
+        "position": "absolute",                    
+        "left": x,
+        "top": y
+    });
+    
+    div.append(elems);
+    $(document.body).append(div);   
 });
