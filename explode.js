9c2fdd2ff9168d72c3d867848ac898300a10a55a
Pat Wilken <patrickwilken28@gmail.com>
2017-04-11 21:27:07 -0400

    Default Checking
    - Checks for defaults on explode.setup
    - Fixed a few spelling mistakes in main.js

--- explode.js
+++ explode.js
@@ -2,12 +2,13 @@ var explode = {classes: [], options: {tooMuch: false}};
 
 explode.setup = function(setup){
     var errors = [];
-    
+    setup = explode.fixit(setup);
+
     if(setup.explodeOn == ""){
         errors.push("explodeOn not found, setting to 'html'");
         setup.explodeOn = "html";
     }
-    
+
     if(setup.min < 1){
         errors.push("min under the minimum, setting to 1");
         setup.min = 1;
@@ -16,7 +17,7 @@ explode.setup = function(setup){
         errors.push("min over the maximum, setting to 500");
         setup.min = 500;
     }
-    
+
     if(setup.max < 10){
         errors.push("max under the minimum, setting to 10");
         setup.max = 10;
@@ -25,7 +26,7 @@ explode.setup = function(setup){
         errors.push("max over the maximum, setting to 10,000");
         setup.max = 10000;
     }
-    
+
     if(setup.possibilities < 1){
         errors.push("possibilities under the minimum, setting to 1");
         setup.possibilities = 1;
@@ -34,12 +35,12 @@ explode.setup = function(setup){
         errors.push("possibilities over the maximum, setting to 500");
         setup.possibilities = 500;
     }
-    
+
     if(setup.rotation != true && setup.rotation != false){
         errors.push("rotation not found, setting to false");
         setup.rotation = false;
     }
-    
+
     if(setup.duration < 0.1){
         errors.push("duration under the minimum, setting to 0.1");
         setup.duration = 0.1;
@@ -48,7 +49,7 @@ explode.setup = function(setup){
         errors.push("duration over the maximum, setting to 300");
         setup.duration = 300;
     }
-    
+
     if(setup.timingFunction != "linear" && setup.timingFunction != "ease"
     && setup.timingFunction != "ease-in" && setup.timingFunction != "ease-out"
     && setup.timingFunction != "ease-in-out" && setup.timingFunction != "step-start"
@@ -58,17 +59,17 @@ explode.setup = function(setup){
         errors.push("timingFunction not found, setting to 'linear'");
         setup.timingFunction = "linear";
     }
-    
+
     if(setup.appendTo == ""){
         errors.push("appendTo not found, setting to 'document.body'");
         setup.appendTo = "document.body";
     }
-    
+
     if(setup.containerId == ""){
         errors.push("containerId not found, setting to 'explosion-container'");
         setup.containerId = "explosion-container";
     }
-    
+
     if(setup.objectsPerExplosion < 1){
         errors.push("objectsPerExplosion under the minimum, setting to 1");
         setup.objectsPerExplosion = 1;
@@ -77,7 +78,7 @@ explode.setup = function(setup){
         errors.push("objectsPerExplosion over the maximum, setting to 500");
         setup.objectsPerExplosion = 500;
     }
-    
+
     if(setup.maxObjects < setup.objectsPerExplosion){
         errors.push("maxObjects less than objectsPerExplosion, setting to objectsPerExplosion (" + setup.objectsPerExplosion + ")");
         setup.maxObjects = setup.objectsPerExplosion;
@@ -86,7 +87,7 @@ explode.setup = function(setup){
         errors.push("maxObjects more than x20 objectsPerExplosion, setting to objectsPerExplosion (" + (setup.objectsPerExplosion * 20) + ")");
         setup.maxObjects = (setup.objectsPerExplosion * 20);
     }
-    
+
     if(setup.hardwareTest != true && setup.hardwareTest != false){
         errors.push("hardwareTest not found, setting to true");
         setup.hardwareTest = true;
@@ -99,22 +100,22 @@ explode.setup = function(setup){
         errors.push("hardwareMax over the maximum, setting to 10");
         setup.hardwareMax = 10;
     }
-    
+
     if(setup.dimensions.w == 0 || setup.dimensions.h == 0){
         errors.push("dimensions either not found or incorrect, will not use")
         setup.dimensions = {w: 0, h: 0};
     }
-    
+
     if(setup.logging != true && setup.logging != false){
         setup.logging = false;
     }
-    
+
     if(setup.logging == true){
         for(var i=0; i<errors.length; i++){
             console.log("Error: " + errors[i]);
         }
     }
-    
+
     setup.hardwareMax *= 1000;
     explode.options = setup;
     explode.init();
@@ -127,7 +128,7 @@ explode.init = function(){
         bottom: [],
         left: []
     };
-    
+
     var start = new Date().getTime();
     for(var i=0; i<explode.options.possibilities; i++){
         explode.classes.push(Math.floor(Math.random() * explode.options.possibilities + 1));
@@ -172,7 +173,7 @@ explode.init = function(){
             }
             else{
                 if(explode.options.rotation){ styles += "100% {transform: rotate(360deg);"; }
-                
+
                 if(rand.one == 1){
                     styles += "top: " + dirs.top[i] + "px;";
                 }
@@ -193,6 +194,66 @@ explode.init = function(){
     styles += "</style>";
 
     $(document.head).append(styles);
+};
+
+explode.fixit = function(setup){
+    function doesExist(elem){
+        var exist = true;
+
+        if(typeof elem == "undefined" || elem == null)
+            exist = false;
+
+        return exist;
+    }
+
+    if(!doesExist(setup))
+        setup = {};
+    if(!doesExist(setup.explodeOn))
+        setup.explodeOn = "html";
+    if(!doesExist(setup.min))
+        setup.min = 20;
+    if(!doesExist(setup.max))
+        setup.max = 300;
+    if(!doesExist(setup.possibilities))
+        setup.possibilities = 100;
+    if(!doesExist(setup.rotation))
+        setup.rotation = true;
+    if(!doesExist(setup.duration))
+        setup.duration = 2;
+    if(!doesExist(setup.timingFunction))
+        setup.timingFunction = "linear";
+    if(!doesExist(setup.additionalClasses))
+        setup.additionalClasses = [];
+    if(!doesExist(setup.appendTo))
+        setup.appendTo = "body";
+    if(!doesExist(setup.containerId))
+        setup.containerId = "explosion-container";
+    if(!doesExist(setup.objectsPerExplosion))
+        setup.objectsPerExplosion = 10;
+    if(!doesExist(setup.maxObjects))
+        setup.maxObjects = 50;
+    if(!doesExist(setup.htmlElement))
+        setup.htmlElement = "<img src='http://patwilken.me/favicon.ico'/>";
+    if(!doesExist(setup.hardwareTest))
+        setup.hardwareTest = true;
+    if(!doesExist(setup.hardwareMax))
+        setup.hardwareMax = 5;
+    if(!doesExist(setup.dimensions)){
+        setup.dimensions = {
+            w: 48,
+            h: 48
+        };
+    }
+    else{
+        if(!doesExist(setup.dimensions.w))
+            setup.dimensions.w = 48;
+        if(!doesExist(setup.dimensions.h))
+            setup.dimensions.h = 48;
+    }
+    if(!doesExist(setup.logging))
+        setup.logging = true;
+
+    return setup;
 };
 
 $(document).on("click", explode.options.explodeOn, function(e){
@@ -220,9 +281,7 @@ $(document).on("click", explode.options.explodeOn, function(e){
                 elems += " " + explode.options.additionalClasses[j];
             }
             elems += " dir-" + Math.floor(Math.random() * explode.options.possibilities + 1) + "'>";
-            if(explode.options.dontUseCSS){
-                elems += explode.options.htmlElement
-            }
+            elems += explode.options.htmlElement
             elems += "</div>";
         }
         elems += "</div>";
